b8a502afde07915b5b8960e65e2d4c02ad026f91
Ajmal Faris <ajmalfaris.me@gmail.com>
2024-09-30 14:29:19 +0530

    update author.js file to use mongoose

--- models/author.js
+++ models/author.js
@@ -1,7 +1,9 @@
-let authors = [
-    { id: 1, name: "Author One" },
-    { id: 2, name: "Author Two" }
-  ];
-  
-  module.exports = authors;
-  
+const mongoose = require("mongoose");
+
+const authorSchema = new mongoose.Schema({
+  name: { type: String, required: true }
+});
+
+const Author = mongoose.model("Author", authorSchema);
+
+module.exports = Author;
