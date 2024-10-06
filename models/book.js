a7abae3a65b9249d0c8c5abfc1d498154bce64f4
Ajmal Faris <ajmalfaris.me@gmail.com>
2024-10-06 14:59:54 +0530

    Add Book model with schema and timestamps.

--- models/book.js
+++ models/book.js
@@ -1,26 +1,26 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const bookSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true,
-    },
-    author: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Author',
-        required: true,
-    },
-    description: {
-        type: String,
-    },
-    image: {
-        type: String, // URL to the book image
-    },
-    publishedDate: {
-        type: Date,
-    },
-}, { timestamps: true });
+const bookSchema = mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Author",
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+}, {
+  timestamps: true,
+});
 
-const Book = mongoose.model('Book', bookSchema);
-
+const Book = mongoose.model("Book", bookSchema);
 module.exports = Book;
