a3d241871c825667f803a47b549752d9c1cff03a
Ajmal Faris <ajmalfaris.me@gmail.com>
2024-10-06 15:01:06 +0530

    Add Author model with schema and timestamps.

--- models/author.js
+++ models/author.js
@@ -1,9 +1,16 @@
 const mongoose = require("mongoose");
 
-const authorSchema = new mongoose.Schema({
-  name: { type: String, required: true }
+const authorSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  biography: {
+    type: String,
+  },
+}, {
+  timestamps: true,
 });
 
 const Author = mongoose.model("Author", authorSchema);
-
 module.exports = Author;
