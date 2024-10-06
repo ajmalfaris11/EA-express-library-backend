08b0675b2beaecbcc48b8bb93d8eedb7379c89a6
Ajmal Faris <ajmalfaris.me@gmail.com>
2024-10-06 15:09:58 +0530

    Set up Express server and connect to MongoDB.

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cookieParser = require('cookie-parser');
-const dotenv = require('dotenv');
-
-const bookRoutes = require('./routes/bookRoutes');
-const userRoutes = require('./routes/userRoutes');
-const authorRoutes = require('./routes/authorRoutes');
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser");
+const bookRoutes = require("./routes/bookRoutes");
+const authorRoutes = require("./routes/authorRoutes");
+const userRoutes = require("./routes/userRoutes");
 
 dotenv.config();
 
@@ -14,17 +13,17 @@ app.use(express.json());
 app.use(cookieParser());
 
 mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-.then(() => console.log('MongoDB connected'))
-.catch(err => console.error('MongoDB connection error:', err));
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => console.log("MongoDB connected"))
+  .catch(err => console.error("MongoDB connection error:", err));
 
-app.use('/api/books', bookRoutes);
-app.use('/api/authors', authorRoutes);
-app.use('/api/users', userRoutes);
+app.use("/api/books", bookRoutes);
+app.use("/api/authors", authorRoutes);
+app.use("/api/users", userRoutes);
 
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
